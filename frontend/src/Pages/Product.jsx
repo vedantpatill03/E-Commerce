2f7ea0ead6f4a650c9c9ab6bd4401893aa19a68f
vedantpatill03 <vedantspatil03@gmail.com>
2026-08-02 15:51:01 +0530

    Fixed product page

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -6,17 +6,29 @@ import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
 import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 
-const Product = () =>{
-    const {all_product} =useContext(ShopContext);
-    const{productId}=useParams();
-    const product =all_product.find((e)=>e.id === Number(productId));
-    return (
-        <div>
-           <Breadcrum product={product}/> 
-           <ProductDisplay product={product}/>
-           <DescriptionBox/>
-           <RelatedProducts/>
-        </div>
-    )
-}
-export default Product
+const Product = () => {
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+
+  const product = all_product.find(
+    (e) => e.id === Number(productId)
+  );
+
+  console.log("URL Product ID:", productId);
+  console.log("Product Found:", product);
+
+  if (!product) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts />
+    </div>
+  );
+};
+
+export default Product;
